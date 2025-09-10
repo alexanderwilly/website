f47762a5513bd4d12dad7d66df76b17fd54fae96
Alexander Willy Johan <alexanderwillyj@gmail.com>
2025-09-10 15:06:56 +0800

    added vercel

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Router basename="/website/">
+    <Router basename="/">
       <App />
     </Router>
   </StrictMode>,
